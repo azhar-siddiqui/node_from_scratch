f3052547654fbad711d8c371d1aa4f133b4ccec4
Azhar Sayyed <azhartsiddiqui@gmail.com>
2022-07-02 16:48:57 +0530

    video8 package.json

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-const http = require("http");
-// http
-//   .createServer((req, resp) => {
-//     resp.write("<h1>Hello This is Azhar Siddiqui</h1>");
-//     resp.end();
-//   })
-//   .listen(4500);
-
-const runServer = (req, resp) => {
-  resp.write("<h1>Hello Azhar How Are You Today ??</h1>");
-  resp.end();
-};
-
-http.createServer(runServer).listen(4500);
+let colors = require("colors");
+console.log("hello".green);
