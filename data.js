cdc3f98e44b64123ca3b4e848e3c977dd59ebcc3
Azhar Sayyed <azhartsiddiqui@gmail.com>
2022-07-02 21:45:29 +0530

    video 10 Simple and Basic api

---
+++ data.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    Name: "Azhar Aminoddin Sayyed",
+    Contact: "7558380826",
+    Email: "azhartsiddiqui@gmail.com",
+    Address: "Pune Camp",
+  },
+  {
+    Name: "Mohd Anas Ali",
+    Contact: "8987464864",
+    Email: "mohdanasali@gmail.com",
+    Address: "Vishrantwaadi Pune",
+  },
+  {
+    Name: "Rizwaan Sayyed",
+    Contact: "8787254656",
+    Email: "rizansayyed@gmail.com",
+    Address: "Sayyed Nager Pune",
+  },
+];
+module.exports = data;
