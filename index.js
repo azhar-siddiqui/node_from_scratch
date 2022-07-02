4c275f9ba91493329c93de3a32e16927c095f3f0
Azhar Sayyed <azhartsiddiqui@gmail.com>
2022-07-02 09:43:08 +0530

    first Program

---
+++ index.js
@@ -0,0 +1,21 @@
+// const fs = require("fs");
+// fs.writeFileSync("file.pdf", "code step by step");
+
+// console.log(__dirname);
+let n = 5; // row or column count
+// defining an empty string
+let string = "";
+
+for (let i = 1; i <= n; i++) {
+  // external loop
+  for (let j = 1; j <= i; j++) {
+    // internal loop
+    // string += "*";
+    console.log("x");
+  }
+  // newline after each row
+  // string += "\n";
+  console.log("\n");
+}
+// printing the string
+// console.log(string);
